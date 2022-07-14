247cf50afa2adb234dbf330286f19d265f4428b3
elijahsamuels <elijahsamuels@gmail.com>
2022-07-14 08:40:56 -0700

    added scatter chart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
+/* eslint-disable */
+
 import "./App.css";
 import LineChart from "./Charts/LineChart";
 import TimeSeries from "./Charts/TimeSeries";
 import Histogram from "./Charts/Histogram";
 import Weather from "./Charts/NYCTemperature2018";
+import ScatterChart from "./Charts/ScatterChart";
 
 function App() {
   return (
     <div className="App">
-
       <div className="row">
-        <Weather height={400} width={400} />
-				
+        <ScatterChart />
+        <Weather />
+
         <LineChart height={400} width={400}/>
         <TimeSeries height={400} width={400} />
         <Histogram height={400} width={400} />
-
       </div>
     </div>
   );
